4df5352ead41031a433e4d91ed2066072bf92415
Naveen Mohan <naveenmsj@gmail.com>
2023-12-31 15:03:26 +0530

    Initial Commit

---
+++ Delivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Delivery
+//
+//  Created by The Scalers on 31/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Delivery/DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryApp.swift
+//  Delivery
+//
+//  Created by The Scalers on 31/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
